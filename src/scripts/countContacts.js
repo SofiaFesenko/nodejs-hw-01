7ab939a91e64dc2cc6cdc2ae46c13c388818f508
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-10-27 14:46:59 +0200

    commit

--- src/scripts/countContacts.js
+++
@@ -1,9 +0,0 @@
-import { readContacts } from '../utils/readContacts.js';
-
-export const countContacts = async () => {
-    const data = await readContacts()
-    return data.length
-};
-
-const data = await countContacts()
-console.log(data);
