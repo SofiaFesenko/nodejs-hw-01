d24ccf01ae3a23d040a95bb1d3ffb5086db74615
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-11-01 12:58:03 +0200

    hw4 validation

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,15 +1,23 @@
 import { createContact, deleteContact, getAllContacts, getContactById, patchContact, putContact } from "../services/contacts.js"
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from "../utils/parsePaginationParams.js";
+import { parseSortParams } from "../utils/parseSortParams.js";
 
 export const getAllContactsController = async (req, res) => {
-    const contacts = await getAllContacts()
-    res.status(200).json(
-        {
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts
-        }
-    )
+    const { page, perPage } = parsePaginationParams(req.query);
+    const { sortBy, sortOrder } = parseSortParams(req.query)
+    const contacts = await getAllContacts({
+        page,
+        perPage,
+        sortBy,
+        sortOrder
+    });
+
+    res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+    });
 }
 
 export const getContactByIdController = async (req, res) => {
