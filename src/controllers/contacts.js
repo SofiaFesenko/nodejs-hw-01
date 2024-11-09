5ee84a5a96e9eff215623fea664f4ece01e3dea0
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-11-09 13:55:29 +0200

    hw6-email-and-images change

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -122,7 +122,7 @@ export const patchContactController = async (req, res, next) => {
     res.status(200).json({
         status: 200,
         message: `Successfully patched a contact!`,
-        data: contact,
+        data: contact.contact,
     });
 };
 
