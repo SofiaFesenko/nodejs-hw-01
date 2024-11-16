9e7fec9f8484c933e4ff5356f352be4cc126af44
Sofiia Fesenko <sofia.fesen@gmail.com>
2024-11-16 19:42:52 +0200

    hw7 swagger

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -15,9 +15,10 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(30),
-    phoneNumber: Joi.string(),
-    email: Joi.string().email(),
-    isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid('work', 'home', 'personal')
+    name: Joi.string().min(3).max(30).allow(null),
+    phoneNumber: Joi.string().allow(null, ''),
+    email: Joi.string().email().allow(null, ''),
+    isFavourite: Joi.boolean().allow(null, ''),
+    contactType: Joi.string().valid('work', 'home', 'personal').allow(null, ''),
+    photo: Joi.allow(null)
 });
